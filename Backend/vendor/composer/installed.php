e01c918fe2960499b49444a23b02e2fa57c1259b
linhnq9904 <linhnqph46354@fpt.edu.vn>
2026-06-19 15:09:22 +0700

    chore: prepare JWT authentication phase

---
+++ Backend/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'linhgpt/backend',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2ae75aac6b58a4c33844a2f429e14d06c41591e9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v7.1.0',
+            'version' => '7.1.0.0',
+            'reference' => 'b374a5d1a4f1f67fadc2165cdb284645945e2fc0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'linhgpt/backend' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2ae75aac6b58a4c33844a2f429e14d06c41591e9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
